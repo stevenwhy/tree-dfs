26239f4fbe00fc9f31bfa706c3f6f71510917574
stevenwhy <stevenyildirim@gmail.com>
2022-03-29 15:26:09 -0400

    dfs to find count of paths which sum to target

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,30 @@
 fun main() {
 
     val root = TreeNode(12)
-    root.left = TreeNode(7, left=TreeNode(9))
+    root.left = TreeNode(7, left=TreeNode(4))
     root.right = TreeNode(1, left=TreeNode(10), right=TreeNode(5))
+
+    val secondRoot = TreeNode(1)
+    secondRoot.left = TreeNode(7, TreeNode(4), TreeNode(5))
+    secondRoot.right = TreeNode(9, TreeNode(2), TreeNode(7))
     println("Tree has path: ${hasPath(root, 23)}")
     println("Tree has path: ${hasPath(root, 16)}")
+
+    println("Tree paths with sum expect 2: ${countPaths(root, 23)}")
+    println("Tree paths with sum expect 2: ${countPaths(secondRoot, 12)}")
 }
 
+fun countPaths(root: TreeNode?, target: Int): Int {
+    if(root == null) return 0
+    var count = 0
+    if(root.value == target && (root.left == null || root.right == null)) {
+        return 1
+    }
+    count += countPaths(root.left, target - root.value)
+    count += countPaths(root.right, target - root.value)
+    return count
+
+}
 fun hasPath(root: TreeNode?, target: Int): Boolean {
     if(root == null) return false
 
